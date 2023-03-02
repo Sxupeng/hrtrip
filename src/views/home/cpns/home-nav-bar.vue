f5d5a27f06c129ab20ae2ed035ae990a07ff6703
xu peng <s1342090618@163.com>
2023-03-02 21:41:45 +0800

    1

---
+++ src/views/home/cpns/home-nav-bar.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class = 'home-nav-bar'>
+    <div class = "nav-bar">
+      <div class = "title">弘源旅途</div>
+    </div>
+  </div>
+</template>
+
+<style lang = "scss" scoped>
+.nav-bar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 46px;
+  border-bottom: 2px solid #f2f2f2;
+
+  .title {
+    color: var(--primary-color);
+    font-size: 18px;
+    font-weight: 700;
+  }
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,11 +1,56 @@
 <script setup>
+import HomeNavBar from "@/views/home/cpns/home-nav-bar.vue";
 
 </script>
 
 <template>
   <div class = "home">
-    <h2>home</h2>
+    <!--导航栏-->
+    <home-nav-bar></home-nav-bar>
+    <div class = "banner">
+      <img src = "@/assets/img/home/banner.webp" alt = ""/>
+    </div>
+    <div class = "locator">
+      <div class = "city">广州</div>
+      <div class = "position">
+        <span class = "text">我的位置</span>
+        <img src = "@/assets/img/home/icon_location.png" alt = ""/>
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang = "scss" scoped></style>
+<style lang = "scss" scoped>
+.home {
+  .banner {
+    img {
+      width: 100%;
+    }
+  }
+
+  .locator {
+    display: flex;
+    align-items: center;
+    height: 44px;
+    padding: 0 20px;
+
+    .city {
+      flex: 1;
+    }
+
+    .position {
+      width: 74px;
+
+      .text {
+        font-size: 12px;
+      }
+
+      img {
+        width: 18px;
+        height: 18px;
+        margin-left: 5px;
+      }
+    }
+  }
+}
+</style>
