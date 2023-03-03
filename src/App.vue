7761da021dfe8376152c325c7b2b3a80f8517a86
xu peng <s1342090618@163.com>
2023-03-03 20:47:45 +0800

    tab-bar 1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import TabBar from "@/components/tab-bar/tab-bar.vue";
+import {useRoute} from "vue-router";
+
+const route = useRoute();
 </script>
 
 <template>
   <div class = "App">
     <router-view></router-view>
-    <tab-bar></tab-bar>
+    <tab-bar v-if = "!route.meta.hideTabBar"></tab-bar>
   </div>
 </template>
 
