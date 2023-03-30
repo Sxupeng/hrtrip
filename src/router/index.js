bff6e1282fc9ccd1cea596fa332f7880d9f5dd34
xu peng <s1342090618@163.com>
2023-03-30 15:54:08 +0800

    完成

--- src/router/index.js
+++ src/router/index.js
@@ -43,7 +43,7 @@ const router = createRouter({
 		},
 		{
 			path: "/detail/:id",
-			component: () => import("../views/detail/detail.vue"),
+			component: () => import("@/views/detail/detail.vue"),
 		},
 	],
 });
