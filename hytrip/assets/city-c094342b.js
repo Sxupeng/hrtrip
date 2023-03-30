a25fed63ab81ef516dd0ddb4475ae6f7d30654bd
xu peng <s1342090618@163.com>
2023-03-30 15:52:29 +0800

    完成

--- hytrip/assets/city-c094342b.js
+++
@@ -1,1 +0,0 @@
-import{a7 as a}from"./index-2735b714.js";import{m as e}from"./index-868c3cdd.js";let l=()=>e.get({url:"/city/all"});const s=a("city",{state:()=>({searchValue:null,allCity:{},currentCity:{cityName:"广州"}}),actions:{async getCityAll(){let{data:t}=await l();this.allCity=t}}});export{s as u};
