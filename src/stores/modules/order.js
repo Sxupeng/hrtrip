c6c261d9663f062fa566debbdfb4d705d21556ff
xu peng <s1342090618@163.com>
2023-03-30 11:15:45 +0800

    完成

--- src/stores/modules/order.js
+++ src/stores/modules/order.js
@@ -5,6 +5,11 @@ const useOrder = defineStore("order", {
 	state: () => ({
 		list: "all",
 		orderList: [],
+		tabs: [
+			{ title: "全部订单", name: "all" },
+			{ title: "近期订单", name: "recent" },
+			{ title: "待支付", name: "pend" },
+		],
 	}),
 	actions: {
 		async getList() {
