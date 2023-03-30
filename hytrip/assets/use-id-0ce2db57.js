12c44d983ea599cc7445e17ba7e0426804c00694
xu peng <s1342090618@163.com>
2023-03-30 15:50:54 +0800

    完成

--- hytrip/assets/use-id-0ce2db57.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./index-2735b714.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return`${t}-${++r}`}export{u};
--- hytrip/assets/use-refs-09391ae5.js
+++
@@ -1,1 +0,0 @@
-import{l as f,aX as o}from"./index-2735b714.js";function c(){const s=f([]),t=[];return o(()=>{s.value=[]}),[s,e=>(t[e]||(t[e]=r=>{s.value[e]=r}),t[e])]}export{c as u};
